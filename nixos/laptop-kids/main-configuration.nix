d36c4957a9e3a43f270a19214365ad0db566ac18
Davy Campano <dcampano@gmail.com>
2022-02-24 16:01:04 -0500

    Add ssh pubkey and initial Koby config

--- nixos/laptop-kids/main-configuration.nix
+++ nixos/laptop-kids/main-configuration.nix
@@ -75,6 +75,7 @@
     description = "dcampano";
     extraGroups = [ "wheel" "networkmanager" ];
     uid = 1001;
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC49uiq6PNv7h5EYXdwyFDry0hXjCf1F4/yryysxgIqQxRm6BW/V8EDtgS/J01OnygvyBY19IjCbP0SbM03H2FL+L1xPu+c6zfeT8PtXl0eitKpceJiYQjH4l/j3RBrrNL3Fygag53eEljVk0A/GPGgqqMY3fzp6Vmww/dcKLpTIwD2noGaU6Rky+ft6r8xkEU7NnNDuBK3KqGP61qUuWcZl7BVwN3j7vN9fKFqWeiKk6lraJKGblBmMTjKixq+cQSib0XSmO9rn2WW8DWnSKTsnMvbIO/rWYHd/xF3obMbx32tF1nzgnD5YNCVT/dq4XV5gUQ/ZNa1bC5VHOeHOO0kqAfmbv5V5w71Sy/sMrAw1bE37h+SwRm9ZUcy+LAUybwPbJ7IgVAKThH/34BViZRCK65VaU5cKllFxt7ztuYpcnSoO5B0QHDFY+FuuKFxqLo9nD09GT7HXmAhCyJRc+QhrUOtZy44FiLN4YALVH19ITp1NGt0m0ZYKV5TcORIofc= dcampano@nixie"
   };
 
   security.sudo.wheelNeedsPassword = false;
