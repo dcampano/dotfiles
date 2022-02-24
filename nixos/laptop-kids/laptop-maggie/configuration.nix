f106ba059a7b5ed6d3dd31363d0e889451d2d857
dcampano <dcampano@gmail.com>
2022-02-24 14:37:30 -0500

    Initial kids configuration

---
+++ nixos/laptop-kids/laptop-maggie/configuration.nix
@@ -0,0 +1,28 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./../main-configuration.nix
+    ];
+
+  networking.hostName = "laptop-maggie"; # Define your hostname.
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.maggie = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+
+  environment.systemPackages = with pkgs; [
+    wget
+    gnome.lightsoff
+  #   firefox
+  ];
+}
+
