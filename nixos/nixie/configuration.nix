1108c9d29cd92a084327e88cd6454a742fd561d7
Davy Campano <dcampano@gmail.com>
2022-03-03 10:10:07 -0500

    Enable gnupg agent

--- nixos/nixie/configuration.nix
+++ nixos/nixie/configuration.nix
@@ -172,10 +172,11 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "gnome3";
+  };
 
   #
   # File Systems To Mount
