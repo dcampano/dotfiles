210a51f8c82dc4fc313a03b7d00ef0124ec5960b
Davy Campano <dcampano@gmail.com>
2022-03-11 22:19:22 +0000

    Use fetchzip instead of fetchurl

--- nixos/servers/webshare/pkgs/goshs/default.nix
+++ nixos/servers/webshare/pkgs/goshs/default.nix
@@ -2,17 +2,17 @@
 pkgs.stdenv.mkDerivation rec {
   name = "goshs";
   version = "0.1.8";
-  src = pkgs.fetchurl {
+  src = pkgs.fetchzip {
     url = "https://github.com/patrickhener/goshs/releases/download/v${version}/goshs_${version}_Linux_x86_64.tar.gz";
-    sha256 = "0lggrhqna1n39sqfsmny433cd195bc5mfax0xgksgicksxbqv9dm";
+    sha256 = "00d9gvvrfhrwsic5yixld7wkl1b2dg3ccyyjkgxynvs0s19w8w5g";
+    stripRoot = false;
   };
   phases = [ "installPhase" ];
   # probably a better way to extract this stuff that I can research
   # but this works for now
   installPhase = ''
     mkdir -p $out/bin
-    cd $out/bin
-    tar --extract --file=$src goshs
-    chmod +x goshs
+    cp $src/goshs $out/bin/goshs
+    chmod +x $out/bin/goshs
   '';
 }
