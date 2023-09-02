9d094f42f15ed28095ffbdc55c59492571e94160
Davy Campano <dcampano@gmail.com>
2023-09-02 16:01:45 +0000

    Changes for tftpd server to support 23.05 nixos

--- nixos/servers/wireguard-home/configuration.nix
+++ nixos/servers/wireguard-home/configuration.nix
@@ -27,10 +27,7 @@
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
@@ -105,7 +102,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.openssh.permitRootLogin = "yes";
+  services.openssh.settings.PermitRootLogin = "yes";
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
